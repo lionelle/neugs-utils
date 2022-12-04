7d9a67fe5710e21f62327ee6f15193e9704eb072
Albert Lionelle <lionelle@gmail.com>
2022-12-03 19:53:29 -0700

    added SF ignore list

--- neugs_utils/__init__.py
+++ neugs_utils/__init__.py
@@ -1,7 +1,18 @@
-from .alt_grading import TierMasteryJSONTestRunner, tier, COMMON_ONE, COMMON_TWO, COMMON_THREE, COMMON_FOUR
+from .alt_grading import TierMasteryJSONTestRunner, tier, \
+    COMMON_ONE, COMMON_TWO, COMMON_THREE, COMMON_FOUR
 
 __version__ = "0.0.1"
 __doc__ = """
-A simple tool set developed for non-traditional grading and other grading tools within the grade-scope autograder environment.
+A simple tool set developed for non-traditional grading and other grading 
+tools within the grade-scope autograder environment.
 """
 
+# Developed by Mark Miller for Khoury Align SF campus use. Placed for easy access.
+
+common_style_ignore_list = [
+    'E121', # Continuation line under-indented for hanging indent
+    'E126', # Continuation line over-indented for hanging indent
+    'W291', # Trailing whitespace
+    'W503', # Line break before binary operator
+    'W504' # Line break after binary operator
+    ]
