5d16d4a8c194072c318301f7a874155480b14120
Albert Lionelle <albert.lionelle@colostate.edu>
2022-12-12 08:57:22 -0700

    fixed typo in context manager, updated tests

--- tests/examples/test_one.py
+++ tests/examples/test_one.py
@@ -1,8 +1,8 @@
 import os
 import sys
-up1 = os.path.abspath('../..')
-print("Add path for test purposes " + up1)
-sys.path.insert(0, up1)
+#up1 = os.path.abspath('../..')
+#print("Add path for test purposes " + up1)
+#sys.path.insert(0, up1)
 
 
 
@@ -15,22 +15,23 @@ from random import randint
 
 def some_func():
     val = input("this is my input")
-    print(f"this is your input: {val}" )
+    print(f"this is your input: {val}\n{val}" )
 
 class TestOne(unittest.TestCase):
 
-    
     @tier(COMMON_ONE)
     def test_capture(self):
         with Capturing("hello") as output:
             some_func()
-        self.assertEqual(output[0], "hello")
+        self.assertEqual(output[1], "hello")
+   
         
 
     @tier(COMMON_THREE)
     @tags("Learning")
     @number(3.0)
     def test_random(self):
+        print("ehlleloe")
         result = randint(0,100)
         for i in (6, 5, 61):
             self.assertEqual(result, i, "this is my message")
@@ -51,4 +52,5 @@ class TestOne(unittest.TestCase):
 
 
 if __name__ == '__main__':
+    print("wtf")
     unittest.main()
