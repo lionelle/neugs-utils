5cd67a3c3061f7eab4be9e810603f35e24504549
Albert Lionelle <albert.lionelle@colostate.edu>
2023-01-12 17:52:35 -0500

    added run range a common way to generate tests

--- neugs_utils/language_support.py
+++ neugs_utils/language_support.py
@@ -22,3 +22,19 @@ def c_compile(c_files: List[str], out_file:str = "a.out", force:bool = False, co
         if (compiled.stderr):
             return "Code failed to compile or had warnings\n{0}".format(compiled.stderr.decode())
     return ''
+
+
+def run_range(file:str, start : int , end: int) -> str:
+        """Runs subprocess passing in start and end as the first two parameters 
+        
+        Args:
+            file (str): the file / program to run
+            start (int): test id of the start test
+            end (int): test id of the end test
+        Returns:
+            str: items printed to stderr
+        """
+        command = subprocess.run([file, str(start), str(end)], capture_output=True)
+        if (command.stderr):
+            return command.stderr.decode()
+        return ''
