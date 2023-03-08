a0da291353c95d835f771e0f218380276f73e63b
Albert Lionelle <albert.lionelle@colostate.edu>
2023-03-07 20:32:40 -0700

    updated to allow more flexibility for c program

--- neugs_utils/language_support.py
+++ neugs_utils/language_support.py
@@ -4,37 +4,51 @@ from typing import List
 import os
 import subprocess
 
-def c_compile(c_files: List[str], out_file:str = "a.out", force:bool = False, compiler="clang -Wall") -> str:
+def c_compile(c_files: List[str], out_file:str = '', force:bool = False, compiler="clang -Wall") -> str:
     """Compiles using the clang compiler by default. Will only compile if 
     the file doesn't already exist
 
     Args:
         c_files (List[str]): list of input files
-        out_file (str, optional): _the file to save out. Defaults to "a.out".
+        out_file (str, optional): _the file to save out. Defaults to ''(which will create a.out)
         force (bool, optional): Force recompile even if it already exists. Defaults to False.
         compiler (str, optional): the compiler to use with arguments. Defaults to clang -Wall
     Returns:
         str: A list of warnings and errors if they happen.
     """
     if not os.path.isfile(out_file) or force:
-        compile_command = compiler.split() + c_files + ["-o", out_file]
+        if out_file:
+            compile_command = compiler.split() + c_files + ["-o", out_file]
+        else:
+            compile_command = compiler.split() + c_files
+
         compiled = subprocess.run(compile_command, capture_output=True)
         if (compiled.stderr):
             return "Code failed to compile or had warnings\n{0}".format(compiled.stderr.decode())
     return ''
 
 
-def run_range(file:str, start : int , end: int) -> str:
+def run_range(file:str, start : int , end: int, capture_error: bool = True, timeout: int=120) -> str:
         """Runs subprocess passing in start and end as the first two parameters 
         
         Args:
             file (str): the file / program to run
             start (int): test id of the start test
             end (int): test id of the end test
+            capture_error(bool, optional): capture stderr instead of stdout. Defaults to True
+            timeout (int, optional): timeout for the subprocess. Defaults to 120.
         Returns:
             str: items printed to stderr
         """
-        command = subprocess.run([file, str(start), str(end)], capture_output=True)
-        if (command.stderr):
-            return command.stderr.decode()
+        command = subprocess.run([file, str(start), str(end)], 
+                                 capture_output=True, timeout=timeout)
+        try:
+            if capture_error:
+                if command.stderr:
+                    return command.stderr.decode()
+            else:
+                 if command.stdout:
+                    return command.stdout.decode()
+        except TimeoutError:
+            return "Timeout Error"
         return ''
